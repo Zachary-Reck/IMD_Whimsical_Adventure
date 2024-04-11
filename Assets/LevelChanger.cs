d26a4cdc64cf19ed1ce3d28a94dfbf6cd54205a2
drowse2005 <drowse2005@gmail.com>
2024-04-11 15:13:56 -0400

    portal animation

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,27 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger :MonoBehaviour
+{
+    
+public Animator animator;
+
+private int levelToLoad;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(1);
+        }
+    }
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("Fade Out");
+    }
+    public void OnFadeComplete (){
+SceneManager.LoadScene(levelToLoad);
+    }
+}
